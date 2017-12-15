17a9a5eadd5e8e72a51865a8856a96e634fb72a4
Vladimir Ilic <vladimirilic96@hotmail.com>
2017-12-15 21:30:09 +0100

    Navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,23 @@ import { AppComponent } from './app.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { EmployeeService } from './employee/employee.service';
 import { HttpModule } from '@angular/http';
+import { AnnualHolidayRegulationComponent } from './annual-holiday-regulation/annual-holiday-regulation.component';
+import { AnnualHolidayRegulationService } from './annual-holiday-regulation/annualHolidayRegulation.service';
+import { NavigationComponent } from './navigation/navigation.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    EmployeeComponent
+    EmployeeComponent,
+    AnnualHolidayRegulationComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
     HttpModule
   ],
-  providers: [EmployeeService],
+  providers: [EmployeeService,AnnualHolidayRegulationService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
