f5f3024a83d2821a0b5314f346c886f803a5709b
Filip Kuzmanovic <filipkuzmanovic94@gmail.com>
2018-01-03 23:45:29 +0100

    .

--- src/app/professional-qualification/professional-qualification.component.ts
+++ src/app/professional-qualification/professional-qualification.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ProfessionalQualificationService } from './professional-qualification.service';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-professional-qualification',
@@ -8,9 +9,18 @@ import { ProfessionalQualificationService } from './professional-qualification.s
 })
 export class ProfessionalQualificationComponent implements OnInit {
 
+  @ViewChild('f') addPQForm: NgForm;
   professionalQs = [];
 
-  constructor(private _professionalQ: ProfessionalQualificationService) { }
+  selectedRow: number;
+  setClickedRow: Function;
+  addProfQualification = false;
+
+  constructor(private _professionalQ: ProfessionalQualificationService) {
+    this.setClickedRow = function (index) {
+      this.selectedRow = index;
+    }
+  }
 
   ngOnInit() {
     this.getAllProfessionalQualifications();
@@ -20,10 +30,50 @@ export class ProfessionalQualificationComponent implements OnInit {
 
     this._professionalQ.getProfessionalQualification()
       .subscribe(
-          (response: any) => [(this.professionalQs = response)],
-          (error) => console.log(error)
-          );
+      (response: any) => [(this.professionalQs = response)],
+      (error) => console.log(error)
+      );
+  }
 
+  onNext() {
+    if (this.selectedRow == null || this.professionalQs.length - 1 == this.selectedRow) {
+      this.selectedRow = 0;
+      return;
+    }
+    this.selectedRow += 1;
+  }
+
+  onLastorFirst(condition: string) {
+    if (condition == "last") {
+      this.selectedRow = this.professionalQs.length - 1;
+      return;
+    }
+    if (condition == "first") {
+      this.selectedRow = 0;
+      return;
+    }
+  }
+
+  onBack() {
+    if (this.selectedRow == null) {
+      this.selectedRow = 0;
+      return;
+    }
+    if (this.selectedRow == 0) {
+      this.selectedRow = this.professionalQs.length - 1;
+      return;
+    }
+    this.selectedRow -= 1;
+  }
+  resetForm() {
+    this.addPQForm.resetForm();
+  }
+
+  onEditPQ(id) {
+
+  }
+
+  onDelete(id) {
 
   }
 }
