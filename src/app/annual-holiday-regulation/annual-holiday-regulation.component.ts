a706d12e347a36f8104c252b34119637df1a2b02
Vladimir Ilic <vladimirilic96@hotmail.com>
2018-01-03 05:04:48 +0100

    Prikaz Resenja za godisnje na strani zaposleni

--- src/app/annual-holiday-regulation/annual-holiday-regulation.component.ts
+++ src/app/annual-holiday-regulation/annual-holiday-regulation.component.ts
@@ -1,29 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,OnDestroy } from '@angular/core';
 import { AnnualHolidayRegulationService } from './annualHolidayRegulation.service';
+import { MessageService } from '../shared/message.service';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
   selector: 'app-annual-holiday-regulation',
   templateUrl: './annual-holiday-regulation.component.html',
   styleUrls: ['./annual-holiday-regulation.component.css']
 })
-export class AnnualHolidayRegulationComponent implements OnInit {
+export class AnnualHolidayRegulationComponent implements OnInit ,OnDestroy {
 
-  annualHolidayRegulations=[];
+  
+  subscription: Subscription;
+  AHR = {
+    "businessYear": 0,
+    "numOfDays": 0,
+  };
+  mess: string = "22";
 
-  constructor(private annualHolidayRegulationService: AnnualHolidayRegulationService ) { }
-
-  ngOnInit() {
-    this.onGet();
+  constructor(private _AHRS: AnnualHolidayRegulationService, private _messageService: MessageService) {
+    this.subscription = this._messageService.getMessage()
+                                            .subscribe(message => 
+                                                { this.mess = message.text, console.log(this.mess + "  " + message.text), this.onGetAHRbyEmployeeId(message.text); }
+                                              );
+                                              
   }
 
-  onGet() {
-    this.annualHolidayRegulationService.getAnnualHolidayRegulations()
+
+  ngOnInit() {
+  }
+
+  onGetAHRbyEmployeeId(employeeId) {
+    this._AHRS.getAHRByEmployee(employeeId)
       .subscribe(
-        (response:any) => (this.annualHolidayRegulations = response,
-          console.log(this.annualHolidayRegulations)),
-        (error) => console.log(error)
+      (response: any) => [(this.AHR = response)],
+      (error) => console.log(error)
       );
   }
 
+
+  
+  ngOnDestroy() {
+    // unsubscribe to ensure no memory leaks
+    this.subscription.unsubscribe();
+  }
+  
 }
 
