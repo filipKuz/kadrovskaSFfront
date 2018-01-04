137c7d446c47352c2a46187ae246b2d2c093774a
Filip Kuzmanovic <filipkuzmanovic94@gmail.com>
2018-01-04 02:09:29 +0100

    .

--- src/app/professional-qualification/professional-qualification.component.ts
+++ src/app/professional-qualification/professional-qualification.component.ts
@@ -10,16 +10,34 @@ import { NgForm } from '@angular/forms';
 export class ProfessionalQualificationComponent implements OnInit {
 
   @ViewChild('f') addPQForm: NgForm;
+  @ViewChild('fE') editPQForm: NgForm;
   professionalQs = [];
 
   selectedRow: number;
   setClickedRow: Function;
   addProfQualification = false;
+  editProfQualification = false;
+
+  clickedPQid;
+
+  pqNameVar: string;
+  pqDegreeVar: string;
+
+  pq = {
+    "name": "",
+    "qualificationDegree": "",
+    "active": ""
+  }
 
   constructor(private _professionalQ: ProfessionalQualificationService) {
     this.setClickedRow = function (index) {
       this.selectedRow = index;
     }
+  }
+
+  onPopulatePq(name, degree) {
+    this.pq.name = name;
+    this.pq.qualificationDegree = degree;
   }
 
   ngOnInit() {
@@ -65,15 +83,71 @@ export class ProfessionalQualificationComponent implements OnInit {
     }
     this.selectedRow -= 1;
   }
+
+  onPost() {
+    this._professionalQ.postPQ(this.pq)
+      .subscribe(
+      (response: any) => this.professionalQs.push(response),
+      (error) => console.log(error)
+      )
+  }
+
   resetForm() {
     this.addPQForm.resetForm();
   }
 
-  onEditPQ(id) {
+  resetEditForm() {
+    this.editPQForm.resetForm();
+  }
 
+  onSubmit(action) {
+    if (action === "add") {
+      this.onPopulatePq(this.pqNameVar, this.pqDegreeVar);
+      this.onPost();
+      this.addProfQualification = !this.addProfQualification;
+    }if (action === "edit") {
+      this.onPopulatePq(this.editPQForm.value.nameEdit, this.editPQForm.value.degreeEdit);
+      this.onPut();
+      this.editProfQualification = !this.editProfQualification;
+      this.resetEditForm();
+    }
+  }
+
+  onEditPQ(id) {
+    this.resetEditForm();
+    this.clickedPQid=id;
+    this.onGetByPQId(this.clickedPQid);
+    this.editProfQualification = !this.editProfQualification;
+  }
+
+  onPut() {
+    this._professionalQ.editPQ(this.pq, this.clickedPQid)
+      .subscribe(
+      (response: any) => (
+          console.log(response),
+          this.getAllProfessionalQualifications()
+      ),
+      (error) => console.log(error)
+      );
+  }
+
+  onGetByPQId(id) {
+    this._professionalQ.getPQById(id)
+      .subscribe(
+        (response: any) => (
+          console.log(response),
+          this.onPopulatePq(response.name, response.qualificationDegree)
+        ),
+        (error) => console.log(error)
+      );
   }
 
   onDelete(id) {
-
+    this._professionalQ.deletePQ(id).subscribe(
+      (response: any) => (
+          this.getAllProfessionalQualifications()
+      ),
+      (error) => console.log(error)
+      );
   }
 }
--- src/app/professional-qualification/professional-qualification.service.ts
+++ src/app/professional-qualification/professional-qualification.service.ts
@@ -23,4 +23,40 @@ export class ProfessionalQualificationService {
             }
             );
     }
+
+    postPQ(pq) {
+        return this.http.post(this._baseUrl, pq)
+            .map(
+                (response: Response) => {
+                    const data = response.json();
+                    return data;
+                }
+            ).catch(
+                (error: Response) => {
+                    return Observable.throw('Something went wrong');
+                }
+            );
+    }
+
+    getPQById(id: number) {
+        return this.http.get(this._baseUrl + "/" + id)
+            .map(
+                (response: Response) => {
+                    const data = response.json();
+                    return data;
+                }
+            ).catch(
+                (error: Response) => {
+                    return Observable.throw('Something went wrong');
+                }
+            );
+    }
+
+    editPQ(pq: any, id: number) {
+        return this.http.put(this._baseUrl + "/" + id, pq);
+    }
+
+    deletePQ(id) {
+        return this.http.put(this._baseUrl + "/delete/" + id, null);
+    }
 }
