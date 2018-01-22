a0ddc2b24444af49518a584220e337bf63600dc6
Uros Teofanovic <uteofanovic4@gmail.com>
2018-01-22 16:48:41 +0100

    Added company edit dialog

--- src/app/company/company.component.ts
+++ src/app/company/company.component.ts
@@ -4,27 +4,75 @@ import { NgForm } from '@angular/forms';
 import { Subscription } from 'rxjs/Subscription';
 import { MessageService } from '../shared/message.service';
 import { CompanyService } from './company.service';
-import { last } from 'rxjs/operators/last';
+import { CityService } from '../city/city.service';
 
 @Component({
     selector: 'app-company',
-    templateUrl: './company.component.html'
+    templateUrl: './company.component.html',
+    styleUrls: ['./company.component.css']
 })
 export class CompanyComponent implements OnInit {
+    @ViewChild('editForm') editCompanyForm: NgForm;
 
-    company = new Object();
+    company = {
+        'companyId': 0,
+        'name': '',
+        'vat': '',
+        'address': '',
+        'phoneNumber': '',
+        'email': '',
+        'cityId': 0
+    };
+    cities = [];
+    city = new Object();
 
-    constructor(private _companyService: CompanyService) {
+    showEditDialog = false;
+
+    constructor(private _companyService: CompanyService, private _cityService: CityService) {
     }
 
     ngOnInit() {
         this.getOurCompany();
+        this.onPopulateDropDownCity();
     }
 
     getOurCompany() {
         this._companyService.getOurCompany()
             .subscribe(
-            (response: any) => [(this.company = response)],
+            (response: any) => [(this.company = response, this.getCityById(response.cityId))],
+            (error) => console.log(error)
+            );
+    }
+
+    onPopulateDropDownCity() {
+        this._cityService.getAllCities().
+            subscribe(
+            (response: any) => (this.cities = response),
+            (error) => (console.log(error))
+            );
+    }
+
+    getCityById(id) {
+        this._cityService.getCityById(id).
+            subscribe(
+            (response: any) => (this.city = response),
+            (error) => (console.log(error))
+            );
+    }
+
+    onEditCompany() {
+        this.showEditDialog = !this.showEditDialog;
+    }
+
+    onSubmit() {
+        this.onPut();
+        this.showEditDialog = !this.showEditDialog;
+    }
+
+    onPut() {
+        this._companyService.editCompany(this.company, this.company.companyId)
+            .subscribe(
+            (response: any) => (this.getOurCompany()),
             (error) => console.log(error)
             );
     }
